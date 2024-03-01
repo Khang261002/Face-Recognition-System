1ac16a0a0c654eec48f28af7e161044bfef45cf8
user101623 <wxgnoh@shockers.wichita.edu>
2024-02-29 21:44:22 -0600

    Added code for camera connection.

--- Face_Dectection/face_detection.py
+++ Face_Dectection/face_detection.py
@@ -3,8 +3,9 @@ import cv2
 
 #TODO: Load the trained model "haarcascade_frontalface_default.xml" in the Model folder
 trained_face_data = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-#TODO: Connect to a camera to capture videos
 
+#TODO: Connect to a camera to capture videos
+webcam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 #TODO: Make a loop to continuously capture frames from the camera
 # and use the trained model to get the coordinates of the faces
