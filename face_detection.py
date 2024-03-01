3dbd9de1996fa71d27620061229d4f84df564ad6
Khang Duong <sugarkhang02@gmail.com>
2024-02-29 21:07:53 -0600

    TODO tasks

---
+++ face_detection.py
@@ -0,0 +1,12 @@
+# Import necessary libraries
+import cv2
+
+#TODO: Import the trained model "haarcascade_frontalface_default.xml" in the Model folder
+
+#TODO: Connect to a camera to capture videos
+
+
+#TODO: Make a loop to continuously capture frames from the camera
+# and use the trained model to get the coordinates of the faces
+#TODO: Draw a rectangle around faces that are detected
+#TODO: Use waitKey() to exit the loop
