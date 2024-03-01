986098812dee1971437eb4b91169b467cdc26463
KaiChunGoh <kaichungoh@gmail.com>
2024-02-29 22:00:49 -0600

    commit while loop

--- Face_Dectection/face_detection.py
+++ Face_Dectection/face_detection.py
@@ -8,6 +8,10 @@ trained_face_data = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_f
 webcam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 #TODO: Make a loop to continuously capture frames from the camera
+while True:
+    successful_frame_read, frame = webcam.read()
+    grayscaled_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face_coordinates = trained_face_data.detectMultiScale(grayscaled_frame)
 #TODO: Use the trained model to get the coordinates of the faces
 
 #TODO: Draw a rectangle around faces that are detected
