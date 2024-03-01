78696e1c2358b715f7a1c2d9ea6ef9a6d744c92f
YuQingLeong <yxleong1@shockers.wichita.edu>
2024-02-29 21:44:00 -0600

    Load Trained Model File

--- Face_Dectection/face_detection.py
+++ Face_Dectection/face_detection.py
@@ -1,8 +1,8 @@
 # Import necessary libraries
 import cv2
 
-#TODO: Import the trained model "haarcascade_frontalface_default.xml" in the Model folder
-
+#TODO: Load the trained model "haarcascade_frontalface_default.xml" in the Model folder
+trained_face_data = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 #TODO: Connect to a camera to capture videos
 
 
