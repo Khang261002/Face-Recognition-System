c0a3773c89d26ec5133a93745fe39a2fe7492407
Khang Duong <sugarkhang02@gmail.com>
2024-03-07 23:05:50 -0600

    Add crop_and_save function

--- Face_Capture/test.py
+++ Face_Capture/test.py
@@ -1,9 +1,14 @@
 # Import necessary libraries
+from PIL import Image
 import cv2
 import sys
 
 sys.path.append('../')
 from Face_Detection import face_detection
+
+def crop_and_save(frame, coordinate, path):
+    cropped_frame = frame.crop(coordinate)
+    # cropped_frame.save(path)
 
 #TODO: Connect to a camera to capture videos
 webcam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
@@ -14,6 +19,7 @@ while True:
 
     for (x, y, w, h) in face_coordinates:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 255), 5)
+        crop_and_save(frame, (x, y, x + w, y + h), "")
     
     cv2.imshow('Face Detector', frame)
     key = cv2.waitKey(1)
