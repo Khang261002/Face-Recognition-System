45d879c388d3cb3016e44b3cab97da64a2c914fc
Khang Duong <sugarkhang02@gmail.com>
2024-03-18 16:53:03 -0500

    MediaPipe for face detection

---
+++ MediaPipe/Face_Capture/__init__.py
@@ -0,0 +1,1 @@
+"""This file is to mark the directory as a Python package"""
---
+++ MediaPipe/Face_Capture/test.py
@@ -0,0 +1,47 @@
+# Import necessary libraries
+from PIL import Image
+
+import cv2
+import sys
+import os
+
+sys.path.append('../')
+from Face_Detection import face_detection
+
+#TODO: Crop the image using the coordinate provided and save it
+def crop_and_save(frame, coordinate, path, num_images, count=[0]):
+    x, y, w, h = coordinate
+    cropped_frame = frame[y:y + h, x:x + w]
+    im = Image.fromarray(cropped_frame)
+    im.save(path + "img_{}.jpeg".format(count[0]))
+    
+    count[0] += 1
+    if count[0] == num_images:
+        return False
+    else:
+        return True
+
+#TODO: Connect to a camera to capture videos
+webcam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+flag = True
+if not os.path.exists("../Data/"):
+    os.makedirs("../Data/")
+
+while webcam.isOpened() and flag:
+    successful_frame_read, frame = webcam.read()
+    face_coordinates = face_detection.detection(frame)
+
+    for coordinate in face_coordinates:
+        x, y, w, h = coordinate
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 255), 5)
+        flag = crop_and_save(frame, coordinate, "../Data/", 100)
+    
+    cv2.imshow('Face Detector', frame)
+    key = cv2.waitKey(1)
+    # key is -1, no key has been pressed
+    if key != -1:
+        break
+
+# Release the webcam
+webcam.release()
+cv2.destroyAllWindows()
