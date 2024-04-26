5a48713a366c45353d8195f86252974192cb99cd
YuQingLeong <yxleong1@shockers.wichita.edu>
2024-04-25 21:35:59 -0500

    made changes for mac to run mediapipe

--- GUI/source/MediaPipe/Face_Detection/test.py
+++ GUI/source/MediaPipe/Face_Detection/test.py
@@ -1,7 +1,7 @@
 import cv2
 import mediapipe as mp
 
-webcam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+webcam = cv2.VideoCapture(0)
 mp_face_mesh = mp.solutions.face_mesh
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
