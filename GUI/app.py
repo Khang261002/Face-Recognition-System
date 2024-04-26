0dd40a39da6585591c5b8a3c8a015d55afcc6ad0
YuQingLeong <yxleong1@shockers.wichita.edu>
2024-04-25 20:53:47 -0500

    improve render temmplates

--- GUI/app.py
+++ GUI/app.py
@@ -1,11 +1,28 @@
-from flask import Flask, redirect, url_for, request
-from source.MediaPipe.Face_Capture import face_capture
+from flask import Flask, redirect, url_for, request, render_template
+# from source.MediaPipe.Face_Capture import face_capture
 
 app = Flask(__name__)
  
+@app.route('/')
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+@app.route('/checkin.html')
+def checkin():
+    return render_template('checkin.html')
+
+@app.route('/register.html')
+def register():
+    return render_template('register.html')
+
 @app.route('/success/<name>')
 def success(name):
-    face_capture.capture(name)
+    # face_capture.capture(name)
     return 'welcome %s' % name
  
 @app.route('/login', methods=['POST', 'GET'])
